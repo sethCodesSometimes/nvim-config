a61609ca5330b2285b7d71cc50596c8d0416d31a
Seth <sethr.128@proton.me>
2025-07-31 22:17:29 +0100

    bg color of terminal now matches that of editor

--- init.lua
+++ init.lua
@@ -2,3 +2,18 @@
 require("config.lazy")
 vim.o.autochdir = true
 vim.o.shell = "/bin/fish"
+vim.api.nvim_create_autocmd({ "ColorScheme", "VimEnter" }, {
+  callback = function()
+    -- Get background colour of normal buffers
+    local normal = vim.api.nvim_get_hl(0, { name = "Normal", link = false })
+    local bg = normal.bg
+
+    if bg then
+      -- Apply it to terminal-related groups
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = bg })
+      vim.api.nvim_set_hl(0, "TermNormal", { bg = bg })
+      vim.api.nvim_set_hl(0, "TermNormalNC", { bg = bg })
+      vim.api.nvim_set_hl(0, "FloatBorder", { bg = bg }) -- optional: match border bg too
+    end
+  end,
+})
