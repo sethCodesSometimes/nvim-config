f36b7174e3af74f1335abdd24ff541dbf5314c02
Seth <sethr.128@proton.me>
2025-08-10 13:48:22 +0100

    fixed bad snacks keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+return {
+  {
+    -- Only needed if you're not already using which-key in this file
+    "folke/which-key.nvim",
+    opts = function(_, opts)
+      opts.defaults["<leader>h"] = {
+        function()
+          require("snacks.dashboard").open()
+        end,
+        "Open Snacks Dashboard",
+      }
+    end,
+  },
+}
